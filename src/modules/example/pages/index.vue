48159e80dcb7dae1ae82d945d9e06c9ed9f7bf5d
Ling2048 <L2048@outlook.com>
2025-07-01 13:05:39 +0800

    first commit

---
+++ src/modules/example/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <view class="root-container">
+    111
+  </view>
+</template>
+
+<style scoped>
+.root-container {
+  padding: 5rem 2.5rem;
+  text-align: center;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import { createUniBoostPlugins } from 'uni-boost-module';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    uni(),
+    createUniBoostPlugins({ src: 'src/modules' })
+  ],
+})
